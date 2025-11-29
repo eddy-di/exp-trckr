f0b344e6bb04b09f65cd945d452a49f8de79cddd
eddy.di <eddy.di.fint@gmail.com>
2025-11-29 13:28:04 +0600

    remove print

--- core/settings/base.py
+++ core/settings/base.py
@@ -4,5 +4,3 @@ from core.configs import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
     'SECRET_KEY', 'django-insecure-=x)ixlw6-2ln9th@#%c!9hoz=u^n2!1urw0h!$-8-4!=c=@4)#')
-
-print('BASE_DIR:', BASE_DIR)
