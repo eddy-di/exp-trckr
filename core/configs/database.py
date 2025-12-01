0bd1322dedf527540bceb17004b02c4b3e59671d
eddy.di <eddy.di.fint@gmail.com>
2025-12-01 23:50:54 +0600

    delete prints

--- core/configs/database.py
+++ core/configs/database.py
@@ -7,15 +7,8 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-print('BASE_DIR:', BASE_DIR)
-
 environ.Env.read_env(BASE_DIR / ".env")
 
-print('DB_NAME:', env("DB_NAME"))
-print('DB_USER:', env("DB_USER"))
-print('DB_PASS:', env("DB_PASS"))
-print('DB_HOST:', env("DB_HOST"))
-print('DB_PORT:', env("DB_PORT"))
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
