8cbb0c44a43c2de43bfc219862e51a8c0046e48e
eddy.di <eddy.di.fint@gmail.com>
2025-12-04 10:34:58 +0600

    update transaction view

--- apps/finance/views/transaction.py
+++ apps/finance/views/transaction.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter
 from drf_spectacular.types import OpenApiTypes
+from django.db import transaction as db_transaction
 
 from apps.finance.models import Transaction
 from apps.finance.serializers import (
@@ -69,9 +70,31 @@ class TransactionViewSet(ModelViewSet):
             return TransactionCreateSerializer
         return TransactionDetailSerializer
 
+    @db_transaction.atomic
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        transaction_obj = serializer.save(user=self.request.user)
 
+        account = transaction_obj.account
+        amount = transaction_obj.amount
+
+        if transaction_obj.type == Transaction.TransactionType.INCOME:
+            account.balance += amount
+        else:  # EXPENSE
+            account.balance -= amount
+
+        account.save(update_fields=["balance"])
+
+    @db_transaction.atomic
     def perform_destroy(self, instance):
-        # ✅ Soft delete instead of physical delete
+        account = instance.account
+        amount = instance.amount
+
+        # ✅ Reverse the original operation
+        if instance.type == Transaction.TransactionType.INCOME:
+            account.balance -= amount
+        else:  # EXPENSE
+            account.balance += amount
+
+        account.save(update_fields=["balance"])
+
         instance.soft_delete()
